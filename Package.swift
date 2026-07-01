4d665822d4a1d6ead66ef3a92c0a68b128193f6b
Iurii Khvorost <iurii.khvorost@gmail.com>
2026-07-01 13:04:29 +0300

    swift-syntax 603.0.2

--- Package.swift
+++ Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     .library(name: "ReactBridge", targets: ["ReactBridge"])
   ],
   dependencies: [
-    .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "603.0.1")
+    .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "603.0.2")
   ],
   targets: [
     .macro(
